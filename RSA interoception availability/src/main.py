6d0550d94893ce4282483e1c81d3e6683cb30ec9
Nina <nina.besser.ilan@gmail.com>
2026-01-30 10:15:24 +0100

    push all

--- RSA interoception availability/src/main.py
+++ RSA interoception availability/src/main.py
@@ -12,48 +12,47 @@ notoys_rsa_data = rsa_data['no_toys']
 
 
 
-# ib_s = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ib_s.csv'))
-# ibr_s = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ibr_s.csv'))
+ib_s = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ib_s.csv'))
+ibr_s = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ibr_s.csv'))
 
 
-# ib_s_18mo = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ib_s_18mo.csv'))
-# ibr_s_18mo = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ibr_s_18mo.csv'))
+ib_s_18mo = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ib_s_18mo.csv'))
+ibr_s_18mo = pd.read_csv(Path('/Users/nina/Desktop/University of Vienna/PhD projects/python code/interoception-synchrony/RSA interoception availability/interoception data/ibr_s_18mo.csv'))
 
 
 
-# def compare_ids(rsa_dict, csv_df, csv_name):
-#     rsa_ids = set(map(str, rsa_dict.keys()))
-#     if csv_name in {'ib_s', 'ibr_s'}:
-#         csv_ids = set(csv_df['id'].astype(str))
-    
-#     else:
-#         csv_ids = set((csv_df['id']-100).astype(str))
+def compare_ids(rsa_dict, csv_df, csv_name):
+    rsa_ids = {str(k).zfill(2) for k in rsa_dict.keys()}
 
-#     mutual_ids = rsa_ids & csv_ids
+    if csv_name in {'ib_s', 'ibr_s'}:
+        csv_ids = {str(i).zfill(2) for i in csv_df['id']}
+    else:
+        csv_ids = {str(i - 100).zfill(2) for i in csv_df['id']}
 
-#     return mutual_ids
-
-
-# mutual_id_toys_ib_s = compare_ids(toys_rsa_data, ib_s, 'ib_s')
-# mutual_id_toys_ibr_s =compare_ids(toys_rsa_data, ibr_s, 'ibr_s')
-
-# mutual_id_no_toys_ib_s = compare_ids(notoys_rsa_data, ib_s, 'ib_s')
-# mutual_id_no_toys_ibr_s =compare_ids(notoys_rsa_data, ibr_s, 'ibr_s')
+    mutual_ids = rsa_ids & csv_ids
+    return mutual_ids
 
 
-# mutual_id_toys_ib_s_18mo =compare_ids(toys_rsa_data, ib_s_18mo, 'ib_s_18mo')
-# mutual_id_toys_ibr_s_18mo =compare_ids(toys_rsa_data, ibr_s_18mo, 'ibr_s_18mo')
+mutual_id_toys_ib_s = compare_ids(toys_rsa_data, ib_s, 'ib_s')
+mutual_id_toys_ibr_s =compare_ids(toys_rsa_data, ibr_s, 'ibr_s')
 
-# mutual_id_no_toys_ib_s_18mo =compare_ids(notoys_rsa_data, ib_s_18mo, 'ib_s_18mo')
-# mutual_id_no_toys_ibr_s_18mo =compare_ids(notoys_rsa_data, ibr_s_18mo, 'ibr_s_18mo')
+mutual_id_no_toys_ib_s = compare_ids(notoys_rsa_data, ib_s, 'ib_s')
+mutual_id_no_toys_ibr_s =compare_ids(notoys_rsa_data, ibr_s, 'ibr_s')
 
 
-# print(len(mutual_id_toys_ib_s))
-# print(len(mutual_id_toys_ibr_s))
-# print(len(mutual_id_no_toys_ib_s))
-# print(len(mutual_id_no_toys_ibr_s))
+mutual_id_toys_ib_s_18mo =compare_ids(toys_rsa_data, ib_s_18mo, 'ib_s_18mo')
+mutual_id_toys_ibr_s_18mo =compare_ids(toys_rsa_data, ibr_s_18mo, 'ibr_s_18mo')
 
-# print(len(mutual_id_toys_ib_s_18mo))
-# print(len(mutual_id_toys_ibr_s_18mo))
-# print(len(mutual_id_no_toys_ib_s_18mo))
-# print(len(mutual_id_no_toys_ibr_s_18mo))
+mutual_id_no_toys_ib_s_18mo =compare_ids(notoys_rsa_data, ib_s_18mo, 'ib_s_18mo')
+mutual_id_no_toys_ibr_s_18mo =compare_ids(notoys_rsa_data, ibr_s_18mo, 'ibr_s_18mo')
+
+
+print(len(mutual_id_toys_ib_s))
+print(len(mutual_id_toys_ibr_s))
+print(len(mutual_id_no_toys_ib_s))
+print(len(mutual_id_no_toys_ibr_s))
+
+print(len(mutual_id_toys_ib_s_18mo))
+print(len(mutual_id_toys_ibr_s_18mo))
+print(len(mutual_id_no_toys_ib_s_18mo))
+print(len(mutual_id_no_toys_ibr_s_18mo))
